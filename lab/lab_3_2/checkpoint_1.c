40ab6e250d0e115be0bd0bea2b62980b09089ca4
Suthinan Rongphon <suthinan.rpn@gmail.com>
2025-01-03 14:18:10 +0700

    add: checkpoint_1

---
+++ lab/lab_3_2/checkpoint_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("\nEnter a positive integer: ");
+    scanf("%d", &num);
+    int i;
+    int result;
+    for (i = 1; i <= 5; i+=1){
+        result += num;
+        printf("\n%d", result);
+    }
+    return 0;
+}
